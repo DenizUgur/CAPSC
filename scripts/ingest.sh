0a5325e22bc2c2cfe11ad7243407102e3feb5163
Deniz Ugur <deniz343@gmail.com>
2021-02-04 21:43:21 +0300

    update

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+rm -rf development/content/*
+
 $(pwd)/FFmpeg/bin/ffmpeg \
+     -v trace \
      -flags2 +export_mvs \
      -stream_loop -1 -re -i $1 \
      -c:v libx264 \
      -use_template 1 -use_timeline 0 \
      -frag_type every_frame \
-     -seg_duration 8 \
+     -seg_duration 10 \
+     -write_prft 1 \
+     -utc_timing_url "http://time.akamai.com/?iso&ms" \
      -streaming 1 -ldash 1 -tune zerolatency \
      -preset ultrafast \
-     -remove_at_exit 1 \
      -f dash development/content/app.mpd
