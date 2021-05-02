7194181f1926fdf1a4f00ada83c022d53d481e83
Omer Faruk Aladag <ofaladag@gmail.com>
2021-05-02 17:09:34 +0300

    Allow cross origin

--- metadata-feeder/metadata/src/main/java/com/ozu/metadata/HomeController.java
+++ metadata-feeder/metadata/src/main/java/com/ozu/metadata/HomeController.java
@@ -5,12 +5,14 @@ import java.time.LocalTime;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
 
 @RestController
+@CrossOrigin
 public class HomeController {
 
 	@GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
